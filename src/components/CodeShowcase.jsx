89b82658409bfe2fae0463de0f84969c2832d1db
Andy Davis <andy@stowzilla.com>
2026-07-28 16:17:19 -0400

    Fix post.rb scrollbar by trimming attr_accessor line

--- src/components/CodeShowcase.jsx
+++ src/components/CodeShowcase.jsx
@@ -42,7 +42,7 @@ end`,
     code: `class Post < ActiveItem::Base
   self.primary_key = :id
 
-  attr_accessor :id, :user_id, :title, :body, :created_at
+  attr_accessor :id, :user_id, :title, :body
 
   validates :title, presence: true
   before_create { self.id ||= SecureRandom.uuid }
