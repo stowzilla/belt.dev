cc5c6808ce071bdfc6a616f9651df8a1e2a17b0f
Andy Davis <andy@stowzilla.com>
2026-07-28 13:57:34 -0400

    Remove duplicate GitHub/Terraform links from footer
    These links are already prominently featured as CTA buttons in the
    Ready to ride section directly above. Having them twice is excessive.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,6 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-links">
-          <a href="https://github.com/stowzilla/belt" className="footer-link-with-logo" aria-label="Belt on GitHub">
-            <img src="/github-mark-white.svg" alt="GitHub" className="footer-link-logo" />
-            <span>GitHub</span>
-          </a>
-          <a href="https://registry.terraform.io/providers/stowzilla/conveyor-belt/latest" className="footer-link-with-logo" aria-label="Conveyor Belt on Terraform Registry">
-            <img src="/terraform-logo.svg" alt="Terraform" className="footer-link-logo" />
-            <span>Terraform Registry</span>
-          </a>
           <a href="/tutorial">Tutorial</a>
           <a href="https://github.com/stowzilla/belt/issues">Issues</a>
           <a href="https://github.com/stowzilla/belt/blob/main/LICENSE">License (MIT)</a>
