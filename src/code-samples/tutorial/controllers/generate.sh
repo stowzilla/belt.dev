5fc18f4ebb04338cb577a7a4f91e04b9044a1d87
Andy Davis <andy@stowzilla.com>
2026-08-03 18:22:38 -0400

    Update tutorial: fix controller generate output (no manifest update)

--- src/code-samples/tutorial/controllers/generate.sh
+++ src/code-samples/tutorial/controllers/generate.sh
@@ -1,7 +1,4 @@
-belt generate controller completions
+$ belt generate controller completions
 
 #   create  lambda/controllers/api/completions_controller.rb
 #   update  config/routes.rb
-#   update  lambda/lib/routes/api_routes.rb
-#
-# ✓ Controller 'completions' generated!
