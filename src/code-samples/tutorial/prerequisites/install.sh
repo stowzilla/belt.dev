bb5a3610d5bb7b47d7042128b6a1cda4a7e4a2ef
Andy Davis <andy@stowzilla.com>
2026-08-04 18:08:06 -0400

    Tutorial: update Belt version to 0.2.20 in install example

--- src/code-samples/tutorial/prerequisites/install.sh
+++ src/code-samples/tutorial/prerequisites/install.sh
@@ -1,5 +1,5 @@
 $ gem install belt
-Fetching belt-0.2.15.gem
+Fetching belt-0.2.20.gem
 
 ╭────────────────────────────────────────────╮
 │  Belt installed successfully!               │
@@ -12,8 +12,8 @@ Fetching belt-0.2.15.gem
 │  Then: belt new <app_name>                 │
 ╰────────────────────────────────────────────╯
 
-Successfully installed belt-0.2.15
-Parsing documentation for belt-0.2.15
-Installing ri documentation for belt-0.2.15
+Successfully installed belt-0.2.20
+Parsing documentation for belt-0.2.20
+Installing ri documentation for belt-0.2.20
 Done installing documentation for belt after 0 seconds
 1 gem installed
